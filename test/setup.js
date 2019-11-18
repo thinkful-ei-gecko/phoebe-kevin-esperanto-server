9b9050f5d08ac2db6ed6024a751365c9208a0d2a
Kevin Wei <chenghaowei@gmail.com>
2019-11-18 16:29:12 -0500

    update Postgrator config to use URL

--- test/setup.js
+++ test/setup.js
@@ -4,10 +4,6 @@ process.env.JWT_SECRET = 'test-jwt-secret';
 process.env.JWT_EXPIRY = '3m';
 
 require('dotenv').config();
-
-process.env.TEST_DB_URL =
-  process.env.TEST_DB_URL || 'postgresql://cap2@localhost/esperanto-test';
-
 const { expect } = require('chai');
 const supertest = require('supertest');
 
